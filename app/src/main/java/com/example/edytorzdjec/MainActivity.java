4f8ed3eec30cf6a660a6adb56f77f9f8e3860adc
OGMaksiu <mateckimaksymilian@gmail.com>
2022-10-17 12:16:39 +0200

    stworzenie wszystkich seekbar'ow i zrobienie przezroczystosci

--- app/src/main/java/com/example/edytorzdjec/MainActivity.java
+++ app/src/main/java/com/example/edytorzdjec/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.edytorzdjec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekS, seekT, seekRot, seekR, seekG, seekB;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekS = (SeekBar) findViewById(R.id.scaleSeek);
+        seekT = (SeekBar) findViewById(R.id.transitionSeek);
+        seekRot = (SeekBar) findViewById(R.id.rotationSeek);
+        seekR = (SeekBar) findViewById(R.id.rSeek);
+        seekG = (SeekBar) findViewById(R.id.gSeek);
+        seekB = (SeekBar) findViewById(R.id.bSeek);
+        image = (ImageView) findViewById(R.id.obraz);
+
+        //Skalowanie
+        seekS.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                //image.getLayoutParams().height ;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //Przezroczystosc
+        seekT.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                image.setImageAlpha(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
